d6e6eae127dd556de95bcf2cd4ca5ae262c079ec
Ronan-Fritz <123597596+Ronan-Fritz@users.noreply.github.com>
2023-01-25 17:38:26 -0500

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ronan Fritz
+ *        Date: 1/25/23
+ * Description: Lab 2 C++ Assignment
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
